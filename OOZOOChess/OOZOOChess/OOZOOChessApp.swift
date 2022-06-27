2c49a553113cac706f66ecc07ea8bf0ada1fa23f
oozoofrog <oozoofrog@gmail.com>
2022-06-27 15:28:25 +0900

    add OOZOOChess

---
+++ OOZOOChess/OOZOOChess/OOZOOChessApp.swift
@@ -0,0 +1,17 @@
+//
+//  OOZOOChessApp.swift
+//  OOZOOChess
+//
+//  Created by jaychoi on 2022/06/27.
+//
+
+import SwiftUI
+
+@main
+struct OOZOOChessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
