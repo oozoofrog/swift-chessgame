53a33f99b92658c5c6eaf8a21eb0a77e8d7a3373
oozoofrog <oozoofrog@gmail.com>
2022-06-27 17:00:39 +0900

    Board를 Grid로 그림

--- OOZOOChess/OOZOOChess/ContentView.swift
+++ OOZOOChess/OOZOOChess/ContentView.swift
@@ -12,19 +12,36 @@ struct ContentView: View {
     @ObservedObject var model: BoardViewModel
     
     var body: some View {
-        LazyVGrid(columns: verticalGridItems) {
-            LazyHGrid(rows: horizontalGridItems) {
-                Text("A")
+        VStack {
+            Spacer()
+            HStack {
+                Spacer()
+                LazyVGrid(columns: columnGridItems) {
+                    ForEach(0..<model.rankCount) { column in
+                        ForEach(0..<model.fileCount) { row in
+                            LazyHGrid(rows: rowGridItems) {
+                                Text(model.icon(column: column, row: row))
+                                    .font(.largeTitle.monospaced())
+                                    .fixedSize()
+                            }
+                        }
+                    }
+                }
+                .padding()
+                Spacer()
             }
+            Spacer()
+        }.onAppear {
+            model.prepare()
         }
     }
     
-    var verticalGridItems: [GridItem] {
-        []
+    var columnGridItems: [GridItem] {
+        [GridItem](repeating: GridItem(.fixed(24), spacing: 7, alignment: .center), count: model.rankCount)
     }
     
-    var horizontalGridItems: [GridItem] {
-        []
+    var rowGridItems: [GridItem] {
+        [GridItem](repeating: GridItem(.fixed(0), spacing: 0, alignment: .center), count: model.fileCount)
     }
 }
 
--- OOZOOChess/OOZOOChess/Model/BoardViewModel.swift
+++ OOZOOChess/OOZOOChess/Model/BoardViewModel.swift
@@ -10,13 +10,32 @@ import chessthings
 
 final class BoardViewModel: ObservableObject {
     
-    private let board = Board()
+    let fileCount: Int
+    let rankCount: Int
     
-    var display: String { board.display().replacingOccurrences(of: ".", with: " ") }
+    func icon(column: Int, row: Int) -> String {
+        if column > 0 && row > 0 {
+            return board.icon(at: Location(file: board.files[row - 1], rank: board.ranks[column - 1])).description
+        } else if column == 0, row > 0 {
+            return board.files[row - 1].rawValue
+        } else if row == 0, column > 0 {
+            return board.ranks[column - 1].rawValue.description
+        } else {
+            return " "
+        }
+    }
     
     func prepare() {
         board.prepare()
         objectWillChange.send()
     }
+    
+    init() {
+        self.board = Board()
+        self.fileCount = board.files.count + 1
+        self.rankCount = board.ranks.count + 1
+    }
+    
+    private let board: Board
     
 }
